817f8506996056e2557463ada3b5b0a7e31b1cd4
Will Darnell <will@ogsys.com>
2017-03-05 20:42:09 -0800

    added DB context class and seed

---
+++ CoffeeSnobs/CoffeeSnobs/Models/CoffeeSnobsDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeSnobs.Models
+{
+    public class CoffeeSnobsDbInitializer : DropCreateDatabaseIfModelChanges
+    {
+        protected override void Seed(CoffeeSnobsDB context)
+        {
+            context.Reviews.AddOrUpdate(new Campus { CampusId = 1, Name = "Bremerton", Address = "1600 Chester Ave.", City = "Bremerton" });
+            context.Campus.AddOrUpdate(new Campus { CampusId = 2, Name = "Poulsbo", Address = "1000 Olympic College Place NW", City = "Poulsbo" });
+            context.Campus.AddOrUpdate(new Campus { CampusId = 3, Name = "PSNS", Address = "Naval Base Kitsap", City = "Bremerton" });
+          
+            base.Seed(context);
+        }
+    }
+}
