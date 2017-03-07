dc79d0c872a29a78827fccefe9e458d7f6a8e9ea
Monique Jacobs <moniquejacobs@student.olympic.edu>
2017-03-06 23:02:24 -0800

    tweaking

--- CoffeeSnobs/CoffeeSnobs/Controllers/HomeController.cs
+++ CoffeeSnobs/CoffeeSnobs/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CoffeeSnobs.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Learn a little more about what is CoffeeSnobs.com";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Discover how to get in touch with the folks at CoffeeSnobs.com";
 
             return View();
         }
